839401d4e2994be4a65f0da2ef6fd9bdd48965f6
TomasOlsson <tommen89@hotmail.com>
2015-12-05 19:43:32 +0100

    added

---
+++ src/feed/Loader.php
@@ -0,0 +1,31 @@
+<?php
+namespace feed;
+    
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\entity\Attribute;  
+use pocketmine\entity\AttributeManager;
+
+
+
+class Loader extends PluginBase{
+    
+    public function onEnable() {
+        $this->getLogger()->notice("Feed har laddats");
+    }       
+
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+        if(strtolower($command->getName()) === "feed"){
+            if ($sender instanceof Player) {
+                $sender->setFood(20);
+                $sender->sendMessage("Yo mama feed ur face!");
+                return true;
+            } else { $sender->sendMessage("Nah not gonna feed that console"); return true; }
+        } 
+
+        return false;
+    }             
+}
+    
