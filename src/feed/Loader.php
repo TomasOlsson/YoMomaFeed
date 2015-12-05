e64c43595ee91095d22e4ad14c14c7550e59126c
TomasOlsson <tommen89@hotmail.com>
2015-12-05 19:52:37 +0100

    Update Loader.php

--- src/feed/Loader.php
+++ src/feed/Loader.php
@@ -13,14 +13,14 @@ use pocketmine\entity\AttributeManager;
 class Loader extends PluginBase{
     
     public function onEnable() {
-        $this->getLogger()->notice("Feed har laddats");
+        $this->getLogger()->notice("Loading that food sheit!");
     }       
 
     public function onCommand(CommandSender $sender, Command $command, $label, array $args){
         if(strtolower($command->getName()) === "feed"){
             if ($sender instanceof Player) {
                 $sender->setFood(20);
-                $sender->sendMessage("Yo mama feed ur face!");
+                $sender->sendMessage("Yo mama fed ur face!");
                 return true;
             } else { $sender->sendMessage("Nah not gonna feed that console"); return true; }
         } 
