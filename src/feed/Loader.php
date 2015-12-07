edb44028f5e2307722287d525bbe9f81639da3b5
TomasOlsson <tommen89@hotmail.com>
2015-12-07 19:42:09 +0100

    Let there be some color :D

--- src/feed/Loader.php
+++ src/feed/Loader.php
@@ -5,6 +5,7 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\Player;
+use pocketmine\utils\TextFormat;
 
 
 class Loader extends PluginBase{
@@ -22,19 +23,19 @@ class Loader extends PluginBase{
                         $player = $this->getServer()->getPlayer($name);
                         if($player instanceof Player){ 
                             // Send some pointless messeges
-                           $sender->sendMessage("Yo mama fed ".$name." face!");
-                           $player->sendMessage($sender->getName()."s mama fed ur face!");
+                           $sender->sendMessage(TextFormat::BLUE . "Yo mama fed ".$name." face!");
+                           $player->sendMessage(TextFormat::BLUE . $sender->getName()." mama fed ur face!");
                            // set food to 20
                            $player->setFood(20);
                             return true;
-                        } else{ $sender->sendMessage("Player ain't online!"); return true; }  
-                    } else { $sender->sendMessage("Ain't nobody got that permissions!"); return true; }
+                        } else{ $sender->sendMessage(TextFormat::BLUE . "Player ain't online!"); return true; }  
+                    } else { $sender->sendMessage(TextFormat::DARK_RED . "Ain't nobody got that permissions!"); return true; }
                 } else { // If args is missing set your own food to 20
-                    $sender->sendMessage("Yo mama fed ur face!");
+                    $sender->sendMessage(TextFormat::BLUE . "Yo mama fed ur face!");
                     $sender->setFood(20);
                     return true;   
                 }
-            } else { $sender->sendMessage("Nah not gonna feed that console"); return true; }
+            } else { $sender->sendMessage(TextFormat::DARK_RED . "Nah not gonna feed that console"); return true; }
         } 
         return false;
     }             
