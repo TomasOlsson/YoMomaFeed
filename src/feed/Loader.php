268b7c751a45683b4a1ced30763907c200d822c8
TomasOlsson <tommen89@hotmail.com>
2015-12-07 14:32:17 +0100

    Update Loader.php

--- src/feed/Loader.php
+++ src/feed/Loader.php
@@ -23,7 +23,7 @@ class Loader extends PluginBase{
                         if($player instanceof Player){ 
                             // Send some pointless messeges
                            $sender->sendMessage("Yo mama fed ".$name." face!");
-                           $player->sendMessage("Yo mama fed ur face!");
+                           $player->sendMessage($sender->getName()."s mama fed ur face!");
                            // set food to 20
                            $player->setFood(20);
                             return true;
